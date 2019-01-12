e33a5d48d612ee8b37f3980a7de160ff3d53c03e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-12 19:00:20 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -27,7 +27,7 @@ describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    expect('hello')
+    logShout('hello')
 
     expect(spy).toHaveBeenCalledWith('HELLO')
 
