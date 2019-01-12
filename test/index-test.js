04ed9a95d44d6b549cba381450ccd173a24dfa72
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-12 18:29:04 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,6 +9,10 @@ describe('shout(string)', function() {
   })
 })
 
+function whisper(string){
+  return string.toLowerCase()
+}
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
