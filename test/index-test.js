c12585149c8cd8db3622b51f41bc8d1bcb8c9834
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-12 19:18:14 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -65,7 +65,7 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
- 
+
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
   })
