05741ef5006d72fea15fb59ed39d1fb26c06a04b
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-12 18:27:31 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,8 @@
+
 function shout(string){
-  return(string)
+  return string.toUpperCase()
 }
+
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
